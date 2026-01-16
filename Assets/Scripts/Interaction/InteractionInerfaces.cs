6cd1efd02e9218d66f5b3a18608b7412eda42cb2
machamy <macham2001@gmail.com>
2026-01-17 03:23:58 +0900

    feat : 상호작용 인터페이스

---
+++ Assets/Scripts/Interaction/InteractionInerfaces.cs
@@ -0,0 +1,24 @@
+﻿namespace Interaction
+{
+    public interface IClcikkable
+    {
+        void OnClick();
+    }
+    public interface IPressable
+    {
+        void OnPress();
+    }
+
+    public interface ICancellable
+    {
+        void OnCancel();
+    }
+    public interface IHoldable
+    {
+        void OnHold();
+    }
+    public interface IInteractable : IClcikkable, IPressable, ICancellable, IHoldable
+    {
+        
+    }
+}
