e1b02edf1d340aef646941be2d63ca4b7f0f3a36
GongMinGi <komki@naver.com>
2026-01-17 21:25:53 +0900

    fix: reward ui 남은 선택 횟수 텍스트 누락 해결

--- Assets/Scripts/UI/Encounter/EncounterUI.cs
+++ Assets/Scripts/UI/Encounter/EncounterUI.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Threading;
 using Core;
+using Cysharp.Threading.Tasks;
+using Database.Generated;
 using UnityEngine;
 
 namespace UI.Encounter
 {
     public class EncounterUI : MonoBehaviour
     {
+        [SerializeField] EncounterImagePanel DeliveryEncounterPanel;
+        [SerializeField] EncounterImagePanel secondEncounterPanel;
+
+        private bool encounterEnded = false;
         private void Awake()
         {
             UIManager.Instance.EncounterUI = this;
             gameObject.SetActive(false);
+        }
+
+        public async UniTask ShowAsync(StageInfo stageInfo, CancellationToken cancellationToken = default)
+        {
+            if (stageInfo.Stage == 5 || stageInfo.Stage == 15)
+            {
+               // 납품 강화 이벤트
+            }
+            else if(stageInfo.Stage == 10 || stageInfo.Stage == 20)
+            {
+                // 흑종원 이벤트
+            }
+
+            while(encounterEnded == false)
+            {
+                await UniTask.Yield(cancellationToken); 
+            }
+
         }
 
         private void OnEnable()
@@ -20,6 +46,11 @@ namespace UI.Encounter
         private void OnDisable()
         {
             
+        }
+
+        public void OnEncounterClick()
+        {
+
         }
 
         public void Hide()
