6eac9a532782c8532398ae56f61028b2a66d24a2
Ping-guin <stanchoi2015@gmail.com>
2026-01-18 11:25:22 +0900

    feat : Encounter UI 작업 (흑종원 사찰 / 나를 골라라)

--- Assets/Scripts/UI/Encounter/DeliveryEncounterPanel.cs
+++ Assets/Scripts/UI/Encounter/DeliveryEncounterPanel.cs
@@ -13,14 +13,18 @@ struct EncounterStruct
     public Sprite encounterImage;
     public Sprite encounterResultImage;
     public string buttonText;
+    public Color btnColor;
+    public TMP_FontAsset font;
 
-    public EncounterStruct(GameplayTag targetTag, int upgradeValue, Sprite encounterImage, Sprite encounterResult, string buttonText)
+    public EncounterStruct(GameplayTag targetTag, int upgradeValue, Sprite encounterImage, Sprite encounterResult, string buttonText, Color buttonColor, TMP_FontAsset font)
     {
         this.targetTag = targetTag;
         this.upgradeValue = upgradeValue;
         this.encounterImage = encounterImage;
         this.encounterResultImage = encounterResult;
         this.buttonText = buttonText; 
+        this.btnColor = buttonColor;
+        this.font = font;
     }
 }
 
@@ -28,6 +32,8 @@ public class DeliveryEncounterPanel : MonoBehaviour
 {
     [SerializeField] private Image displayImage1;
     [SerializeField] private Image displayImage2;
+    [SerializeField] private Button leftButton;
+    [SerializeField] private Button rightButton;
     [SerializeField] private TextMeshProUGUI leftButtonText;
     [SerializeField] private TextMeshProUGUI rightButtonText;
     [SerializeField] private Sprite meatShopImage;
@@ -40,6 +46,23 @@ public class DeliveryEncounterPanel : MonoBehaviour
     [SerializeField] private DeliveryEncounterResult deliveryEncounterResult;
     [SerializeField] private int upgradeValue;
     [SerializeField] List<EncounterStruct> encounterStructs;
+
+    [Space(10)]
+    [Header("Button Colors")]
+    [SerializeField] private Color meatBtnColor;
+    [SerializeField] private Color fishBtnColor;
+    [SerializeField] private Color vegetableBtnColor;
+
+    [Space(10)]
+    [Header("Fonts")]
+    [SerializeField] private TMP_FontAsset meatFont;
+    [SerializeField] private TMP_FontAsset fishFont;
+    [SerializeField] private TMP_FontAsset vegetableFont;
+
+    [Space(10)]
+    [SerializeField] private GameObject leftCheck;
+    [SerializeField] private GameObject rightCheck;
+
     private Sprite selectedResultImg;
     private EncounterUI encounterUI;
     private int leftIndex;
@@ -56,14 +79,18 @@ public class DeliveryEncounterPanel : MonoBehaviour
         this.leftIndex = selectedA;
         this.rightIndex = selectedB;
         encounterStructs = new List<EncounterStruct>();
-        encounterStructs.Add(new(AllGameplayTags.Ingredient.Meat.Get(), 10, meatShopImage, meatResultImage,  "정육점을 믿기"));
-        encounterStructs.Add(new(AllGameplayTags.Ingredient.Vegetable.Get(), 10, vegetableShopImage, vegetableResultImage ,"채소 가게를 믿기"));
-        encounterStructs.Add(new(AllGameplayTags.Ingredient.Seafood.Get(), 10, fishShopImage, fishResultImage, "수산시장을 믿기"));
+        encounterStructs.Add(new(AllGameplayTags.Ingredient.Meat.Get(), 10, meatShopImage, meatResultImage,  "정육점을 믿기", meatBtnColor, meatFont));
+        encounterStructs.Add(new(AllGameplayTags.Ingredient.Vegetable.Get(), 10, vegetableShopImage, vegetableResultImage ,"채소 가게를 믿기", vegetableBtnColor, vegetableFont));
+        encounterStructs.Add(new(AllGameplayTags.Ingredient.Seafood.Get(), 10, fishShopImage, fishResultImage, "수산시장을 믿기", fishBtnColor, fishFont));
 
         displayImage1.sprite = encounterStructs[selectedA].encounterImage;
         displayImage2.sprite = encounterStructs[selectedB].encounterImage;
         leftButtonText.text = encounterStructs[selectedA].buttonText;
         rightButtonText.text = encounterStructs[selectedB].buttonText;
+        leftButton.GetComponent<Image>().color = encounterStructs[selectedA].btnColor;
+        rightButton.GetComponent<Image>().color = encounterStructs[selectedB].btnColor;
+        leftButtonText.font = encounterStructs[selectedA].font;
+        rightButtonText.font = encounterStructs[selectedB].font;
     }
     void Update()
     {
@@ -76,6 +103,8 @@ public class DeliveryEncounterPanel : MonoBehaviour
         upgradeValue = encounterStructs[leftIndex].upgradeValue;
         selectedResultImg = encounterStructs[leftIndex].encounterResultImage;
         upgradeSelected = true;
+        rightCheck.SetActive(false);
+        leftCheck.SetActive(true);
     }
 
     public void OnClickRightButton()
@@ -84,6 +113,8 @@ public class DeliveryEncounterPanel : MonoBehaviour
         upgradeValue = encounterStructs[rightIndex].upgradeValue;
         selectedResultImg = encounterStructs[rightIndex].encounterResultImage;
         upgradeSelected = true;
+        leftCheck.SetActive(false);
+        rightCheck.SetActive(true);
     }
 
     public void OnClickSelectButton()
